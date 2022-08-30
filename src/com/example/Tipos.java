aea963952a124d4eed6398664daa5eae01bacf11
marc1896 <marco123107@hotmail.com>
2022-08-30 18:17:16 -0500

    Ejercicio sesión 1, se creo una clase *Tipos* dentro de un paquete *com.example*, tambien se creo el metodo main y se imprimio todos los datos que se vieron en la sesión 1.

---
+++ src/com/example/Tipos.java
@@ -0,0 +1,45 @@
+package com.example;
+
+public class Tipos {
+    public static void main(String[] args) {
+        //1.- numericos
+        //1.1- enteros
+        byte numByte = 1;
+        short numShort = 10;
+        int numInt = 30;
+        long numLong = 100;
+
+        //1.2- decimales
+        float numFloat = 5.5f;
+        double numDouble = 10.5d;
+
+        //2.- booleano
+        boolean numFalse = false;
+        boolean numTrue = true;
+
+        //3.- texto
+        char numChar = 'A';
+        String numString = "Hola";
+
+        System.out.println("Tipo de dato byte: " + numByte);
+        System.out.println("------------------------------------------");
+        System.out.println("Tipo de dato short: " + numShort);
+        System.out.println("------------------------------------------");
+        System.out.println("Tipo de dato int: " + numInt);
+        System.out.println("------------------------------------------");
+        System.out.println("Tipo de dato long: " + numLong);
+        System.out.println("------------------------------------------");
+        System.out.println("Tipo de dato float: " + numFloat);
+        System.out.println("------------------------------------------");
+        System.out.println("Tipo de dato double: " + numDouble);
+        System.out.println("------------------------------------------");
+        System.out.println("Tipo de dato boolean(true): " + numTrue);
+        System.out.println("------------------------------------------");
+        System.out.println("Tipo de dato boolean(false): " + numFalse);
+        System.out.println("------------------------------------------");
+        System.out.println("Tipo de dato char: " + numChar);
+        System.out.println("------------------------------------------");
+        System.out.println("Tipo de dato String: " + numString);
+        System.out.println("------------------------------------------");
+    }
+}
